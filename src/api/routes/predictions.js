cc596e132e73e333b55c6037bf5bc13642f1cf57
Sol Abrahams <solomonabrahams100@gmail.com>
2020-09-07 23:44:38 +0100

    auth enabled

--- src/api/routes/predictions.js
+++ src/api/routes/predictions.js
@@ -2,13 +2,12 @@ const updatePrediction = require('../../services/predictions').updatePrediction
 
 exports.predictions = (express) => {
   express.post('/updateprediction', (req,res) => {
-    /*if (!req.session.passport) {
+    if (!req.session.passport) {
       res.status(401)
       res.json()
       return
-    }*/
-    //const username = req.session.passport.user
-    const username = 'sol'
+    }
+    const username = req.session.passport.user
     const home_pred = req.body.home_pred
     const away_pred = req.body.away_pred
     const game_id = req.body.game_id
@@ -16,5 +15,8 @@ exports.predictions = (express) => {
     updatePrediction(username, home_pred, away_pred, game_id)
 
     res.json();
+  })
+  express.get('/getpredictions', (req, res) => {
+    // https://footballapi.pulselive.com/football/compseasons/363/gameweeks
   })
 }
