161b8f939398b487dabe9ee7f722a3a798ab21b0
sol <solomonabrahams100@gmail.com>
2021-01-09 13:41:01 +0000

    MAIN-16 #deploy

--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
+const env = require('dotenv').config()['parsed'] || process.env;
 
 exports.mongoose = async () => {
-  const uri = 'mongodb://127.0.0.1:27017/simple-predictions-api'
-  //const uri = 'mongodb+srv://compass:solaustin@simple-predictions-api-gpv4x.gcp.mongodb.net/simple-predictions-api?retryWrites=true&w=majority'
+  const uri = env.MONGO_URI
   const connection = await mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
   return connection.connection.db;
 };
