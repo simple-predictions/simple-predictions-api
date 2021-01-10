7b19dd3c586f0feefa577760b296526a66ab9907
sol <solomonabrahams100@gmail.com>
2021-01-10 20:36:41 +0000

    updateFixtures updates previous games too

--- src/services/games.js
+++ src/services/games.js
@@ -4,7 +4,7 @@ const Match = require('../models/user').match
 exports.games = () => {
   const options = {
     host: 'cors-anywhere.herokuapp.com',
-    path: '/https://footballapi.pulselive.com/football/fixtures?comps=1&teams=1,2,131,43,4,6,7,34,9,26,10,11,12,23,18,20,21,36,25,38&compSeasons=363&page=0&pageSize=1000&sort=asc&statuses=U,L&altIds=true',
+    path: '/https://footballapi.pulselive.com/football/fixtures?comps=1&teams=1,2,131,43,4,6,7,34,9,26,10,11,12,23,18,20,21,36,25,38&compSeasons=363&page=0&pageSize=1000&sort=asc&statuses=C,U,L&altIds=true',
     method: 'GET',
     port: 443,
     headers: { 'Origin': 'https://www.premierleague.com' }
