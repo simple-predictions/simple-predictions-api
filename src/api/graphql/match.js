c13d27e8140aa8386b0b2ae21d60ed53837fbd84
sol <solomonabrahams100@gmail.com>
2021-02-18 13:44:44 +0000

    match sorting added by kick off time

--- src/api/graphql/match.js
+++ src/api/graphql/match.js
@@ -1,8 +1,20 @@
 const { MatchTC } = require('../../models/user.js')
 
+const sortMatches = matches => {
+  return matches.sort((a, b) => new Date(a.kick_off_time) - new Date(b.kick_off_time))
+}
+
 exports.MatchQuery = {
   matchById: MatchTC.getResolver('findById'),
-  matchByIds: MatchTC.getResolver('findByIds'),
+  matchByIds: MatchTC.getResolver('findByIds').wrapResolve(next => async rp => {
+    rp.projection.kick_off_time = true
+    const res = await next(rp)
+    return sortMatches(res)
+  }),
   matchOne: MatchTC.getResolver('findOne'),
-  matchMany: MatchTC.getResolver('findMany')
+  matchMany: MatchTC.getResolver('findMany').wrapResolve(next => async rp => {
+    rp.projection.kick_off_time = true
+    const res = await next(rp)
+    return sortMatches(res)
+  })
 }
