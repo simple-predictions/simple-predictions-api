55c8cf097a11fe06384a043e233c17fec6487805
sol <solomonabrahams100@gmail.com>
2021-02-18 14:37:23 +0000

    added for gameweek 0

--- src/api/graphql/match.js
+++ src/api/graphql/match.js
@@ -1,4 +1,15 @@
 const { MatchTC } = require('../../models/user.js')
+const { getGameweek } = require('../../services/predictions')
+
+const prepareMatchesQuery = async rp => {
+  rp.projection.kick_off_time = true
+
+  if (rp.args.filter.gameweek === 0) {
+    rp.args.filter.gameweek = await getGameweek()
+  }
+
+  return rp
+}
 
 const sortMatches = matches => {
   return matches.sort((a, b) => new Date(a.kick_off_time) - new Date(b.kick_off_time))
@@ -7,13 +18,13 @@ const sortMatches = matches => {
 exports.MatchQuery = {
   matchById: MatchTC.getResolver('findById'),
   matchByIds: MatchTC.getResolver('findByIds').wrapResolve(next => async rp => {
-    rp.projection.kick_off_time = true
+    rp = await prepareMatchesQuery(rp)
     const res = await next(rp)
     return sortMatches(res)
   }),
   matchOne: MatchTC.getResolver('findOne'),
   matchMany: MatchTC.getResolver('findMany').wrapResolve(next => async rp => {
-    rp.projection.kick_off_time = true
+    rp = await prepareMatchesQuery(rp)
     const res = await next(rp)
     return sortMatches(res)
   })
