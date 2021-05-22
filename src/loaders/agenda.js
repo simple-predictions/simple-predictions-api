4fcc930edfd95cc0546c48f7a2424c4b3ffef944
sol <solomonabrahams100@gmail.com>
2021-05-22 12:09:33 +0100

    agenda increase interval from 5 seconds to 2 mins

--- src/loaders/agenda.js
+++ src/loaders/agenda.js
@@ -2,7 +2,8 @@ const Agenda = require('agenda')
 
 exports.agenda = ({ mongoConnection }) => {
   return new Agenda({
-    mongo: mongoConnection
+    mongo: mongoConnection,
+    processEvery: '2 minutes'
   })
   /**
    * This voodoo magic is proper from agenda.js so I'm not gonna explain too much here.
