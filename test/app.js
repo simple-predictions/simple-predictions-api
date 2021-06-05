09bd163a37d167c612ccc500bfd33980d015e279
sol <solomonabrahams100@gmail.com>
2021-06-05 22:10:41 +0100

    testing updates

--- test/app.js
+++ test/app.js
@@ -4,13 +4,8 @@ const mongod = new MongoMemoryServer({ binary: { version: '4.4.6' } });
 
 const chai = require('chai')
 const chaiHttp = require('chai-http')
-const getUserInfo = require('../src/services/auth').getUserInfo
-const User = require('../src/models/user').user
-const di = require('../src/loaders/di').di
 
 const mongoose = require('mongoose')
-
-const { createMiniLeague } = require('../src/services/minileague')
 
 const connect = async () => {
     const uri = await mongod.getUri();
@@ -41,34 +36,17 @@ const clearDatabase = async () => {
 }
 
 // const server = require('../app').server
-// const should = chai.should()
-
+chai.should()
 chai.use(chaiHttp)
 // Our parent block
-before(async () => {
-    await connect();
-});
+before(async () => await connect());
+
+afterEach(async() => await clearDatabase());
 
 after(async () => await disconnnect());
 
 describe("Get all users", function() {
     it("should return user info", function(done) {
         done()
-    })
-})
-
-describe('user', function() {
-    it("can be created", async function() {
-        await User.register(new User({ username: 'sol', email: 'solomonabrahams100@gmail.com' }), 'testpass')
-    })
-    describe('read', function() {
-        before(async () => {
-            await User.register(new User({ username: 'sol', email: 'solomonabrahams100@gmail.com' }), 'testpass').catch(err => {
-                // Ignore error
-            })
-        })
-        it("can be read", async function() {
-            await getUserInfo('sol')
-        })
     })
 })
---
+++ test/user.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+
+const User = require('../src/models/user').user
+
+const { createMiniLeague } = require('../src/services/minileague');
+const { resetPassword, createNewPassword, getUserInfo } = require('../src/services/auth');
+const { expect } = require('chai');
+
+describe('user', function() {
+    it("can be created", async function() {
+        await User.register(new User({ username: 'sol', email: 'solomonabrahams100@gmail.com' }), 'testpass')
+    })
+    describe('read', function() {
+        beforeEach(async () => {
+            await User.register(new User({ username: 'sol', email: 'solomonabrahams100@gmail.com' }), 'testpass').catch(err => {
+                // Ignore error as user may have already been created
+            })
+        })
+        it("can be read", async function() {
+            await getUserInfo('sol')
+        })
+        it("can reset password", async function() {
+            const emailRes = await resetPassword('sol')
+            expect(emailRes).to.equal('Email sent')
+
+            await User.updateOne({ username: 'sol' }, { verification_token: 'stub' })
+
+            const passwordRes = await createNewPassword('sol', 'stub', 'newpassword')
+            expect(passwordRes).to.equal('Password updated. Please login using your new password.')
+        })
+    })
+})
