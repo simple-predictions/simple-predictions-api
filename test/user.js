061cd010bf25f45b8747d82684876e1e2e9d115c
sol <solomonabrahams100@gmail.com>
2021-06-06 20:45:29 +0100

    new friend tests

--- test/user.js
+++ test/user.js
@@ -4,6 +4,7 @@ const User = require('../src/models/user').user
 
 const { createMiniLeague } = require('../src/services/minileague');
 const { resetPassword, createNewPassword, getUserInfo, auth_user } = require('../src/services/auth');
+const { addFriend } = require('../src/services/friends')
 const { expect, assert } = require('chai');
 
 describe('user', function() {
@@ -33,6 +34,25 @@ describe('user', function() {
         })
         it("cannot reset password with incorrect verification token", async function() {
             await createNewPassword('sol', 'incorrecttoken', 'newpassword').should.be.rejectedWith("Verification token doesn't match")
+        })
+
+        describe("can handle friends", function() {
+            beforeEach(async () => {
+                await User.register(new User({ username: 'friend1', email: 'friend@gmail.com' }), 'testpass').catch(err => {
+                    // Ignore error as user may have already been created
+                })
+            })
+            it("can add friends", async function() {
+                await addFriend('sol', 'friend1')
+            })
+            it("cannot add friends that don't exist", async function() {
+                await addFriend('sol', 'fakefriend').should.be.rejectedWith('Username not found')
+            })
+            it("cannot add friends twice", async function() {
+                await User.updateOne({ username: 'sol'}, { friends: [] })
+                await addFriend('sol', 'friend1')
+                await addFriend('sol', 'friend1').should.be.rejectedWith('You are already following friend1')
+            })
         })
     })
     describe('that does not exist', function() {
