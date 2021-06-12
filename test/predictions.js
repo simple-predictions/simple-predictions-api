ad94991ff5aade61b1aedcc93927b6fef22fefd7
sol <solomonabrahams100@gmail.com>
2021-06-12 22:27:41 +0100

    predictions tests

--- test/predictions.js
+++ test/predictions.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-const { updatePrediction, updateManyPredictions } = require('../src/services/predictions')
+const { updatePrediction, updateManyPredictions, getUserPredictions } = require('../src/services/predictions')
 const predictionModule = require('../src/services/predictions')
 const Match = require('../src/models/user').match
 const User = require('../src/models/user').user
@@ -9,7 +9,7 @@ const sinon = require('sinon')
 describe('predictions', function() {
     beforeEach(async () => {
         await User.register(new User({ username: 'sol', email: 'solomonabrahams100@gmail.com' }), 'testpass')
-        const match = await Match.create({home_team: 'Arsenal', away_team: 'Tottenham'})
+        const match = await Match.create({home_team: 'Arsenal', away_team: 'Tottenham', gameweek: 1})
         this.ctx.currentTest.matchID = match['_id'].toString()
     })
     it("can be created individually", async function() {
@@ -82,6 +82,43 @@ describe('predictions', function() {
         stub.args[2][4].should.equal(false)
         stub.args[2][5].should.equal(true)
         stub.restore()
+    })
+    it('can be updated collectively and retrieved', async function() {
+        const match1 = await Match.create({home_team: 'Team1', away_team: 'Team2', gameweek: 1})
+        const match2 = await Match.create({home_team: 'Team3', away_team: 'Team4', gameweek: 1})
+        const data = [
+            {
+                home_pred: 1,
+                away_pred: 0,
+                banker: false,
+                insurance: false,
+                game_id: this.test.matchID
+            },
+            {
+                home_pred: 1,
+                away_pred: 1,
+                banker: true,
+                insurance: false,
+                game_id: match1._id
+            },
+            {
+                home_pred: 1,
+                away_pred: 0,
+                banker: false,
+                insurance: true,
+                game_id: match2._id
+            }
+        ]
+        await updateManyPredictions('sol', data)
+        
+        const preds = await getUserPredictions('sol', 1, true)
+        preds.data.should.have.length(3)
+        preds.data[0].user_predictions[0].home_pred.should.equal(1)
+        preds.data[0].user_predictions[0].away_pred.should.equal(0)
+        preds.data[1].user_predictions[0].home_pred.should.equal(1)
+        preds.data[1].user_predictions[0].away_pred.should.equal(1)
+        preds.data[2].user_predictions[0].home_pred.should.equal(1)
+        preds.data[2].user_predictions[0].away_pred.should.equal(0)
     })
     it('cannot have a banker and insurance', async function() {
         await updatePrediction('sol', 1, 0, this.test.matchID, true, true).should.eventually.be.rejectedWith('You cannot play an insurance and banker')
