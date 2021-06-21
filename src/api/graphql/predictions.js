841b2d1bd61a5b1ce090bc5de6c30bd174cc3fba
Sol Abrahams <solomonabrahams100@gmail.com>
2021-06-21 18:50:24 +0100

    bug fix - preds updates

--- src/api/graphql/predictions.js
+++ src/api/graphql/predictions.js
@@ -65,11 +65,15 @@ exports.PredictionQuery = {
 exports.PredictionMutation = {
   updatePrediction: {
     type: PredictionTC,
-    args: { matchID: 'String!', home_pred: 'Int!', away_pred: 'Int!', banker: 'Boolean', insurance: 'Boolean' },
+    args: { matchID: 'String!', home_pred: 'Int', away_pred: 'Int', banker: 'Boolean', insurance: 'Boolean' },
     resolve: async (source, args, context, info) => {
       // eslint-disable-next-line camelcase
       const { kick_off_time, gameweek } = (await match.findOne({ _id: args.matchID }).select('kick_off_time gameweek -_id'))
       if (new Date(kick_off_time) < Date.now()) {
+        return
+      }
+
+      if (!(args.home_pred && args.away_pred)) {
         return
       }
 
